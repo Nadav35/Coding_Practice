cc6d0344b9b44a2d972684455763af768ec16e3f
Nadav <nadavnoy@yahoo.com>
2019-02-13 13:18:50 -0800

    modify 2-tables version

--- algoExpert/max_profit_with_k_transactions.js
+++ algoExpert/max_profit_with_k_transactions.js
@@ -1,23 +1,52 @@
 
 
 let maxProfitWithKTransactions = (prices, k) => {
+  // if (prices.length === 0) return 0;
+
+  // let profits = [];
+  // while (profits.push(new Array(prices.length)) < k + 1);
+  // profits.forEach(row => {
+  //   row.fill(0);
+  // });
+
+  // for (let t = 1; t < k + 1; t++) {
+  //   let maxThusFar = -Infinity;
+
+  //   for (let d = 1; d < prices.length; d++) {
+  //     maxThusFar = Math.max(maxThusFar, profits[t - 1][d - 1] - prices[d - 1]);
+  //     profits[t][d] = Math.max(profits[t][d - 1], maxThusFar + prices[d]);
+  //   }
+  // }
+  // console.log(profits[k][prices.length - 1]);
+
+  // with O(n) space
+
   if (prices.length === 0) return 0;
 
-  let profits = [];
-  while (profits.push(new Array(prices.length)) < k + 1);
-  profits.forEach(row => {
-    row.fill(0);
-  });
-
+  let evenProfits = new Array(prices.length).fill(0);
+  let oddProfits = new Array(prices.length).fill(0);
   for (let t = 1; t < k + 1; t++) {
     let maxThusFar = -Infinity;
+    let currentProfits, previousProfits;
+    if (t % 2 === 1) {
+      currentProfits = oddProfits;
+      previousProfits = evenProfits;
+    } else {
+      currentProfits = evenProfits;
+      previousProfits = oddProfits;
+    }
 
     for (let d = 1; d < prices.length; d++) {
-      maxThusFar = Math.max(maxThusFar, profits[t - 1][d - 1] - prices[d - 1]);
-      profits[t][d] = Math.max(profits[t][d - 1], maxThusFar + prices[d]);
+      maxThusFar = Math.max(maxThusFar, previousProfits[d - 1] - prices[d - 1]);
+      currentProfits[d] = Math.max(currentProfits[d - 1], maxThusFar + prices[d]);
     }
   }
-  console.log(profits[k][prices.length - 1]);
+
+  console.log(`even ${evenProfits} and odd ${oddProfits}`);
+  
+  
+
+
 };
 
 maxProfitWithKTransactions([5,11,3,50,60,90], 2);
