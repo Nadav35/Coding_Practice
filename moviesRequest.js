029ec067dedaa2002df704acb5952569bbb7d313
Nadav <nadavnoy@yahoo.com>
2019-02-25 14:30:55 -0800

    finished using fetch()

--- moviesRequest.js
+++ moviesRequest.js
@@ -1,7 +1,38 @@
-let https = require('https');
 
 function fetchData(substr) {
-  console.log(substr);
+  let pageNum = 1;
+  let movieArray = [];
+  let url = 'https://jsonmock.hackerrank.com/api/movies/search/?Title=' + substr + "&page=" + pageNum;
+  // fetch(url)
+  //   .then(res => {
+  //     return res.json();
+      
+      
+  //   }).then(jsonRes => {
+  //     let numPages = jsonRes.total_pages;
+  //     let movies = jsonRes.data;
+      
+  //     movies.forEach(a => {
+  //       movieArray.push(a.Title);
+  //     });
+  //     for (let i = 2; i <= numPages; i++) {
+  //       // console.log('here');
+        
+  //       let newUrl = 'https://jsonmock.hackerrank.com/api/movies/search/?Title=' + substr + "&page=" + i;
+  //       fetch(newUrl)
+  //         .then(res => {
+  //           return res.json();
+  //         }).then(res2 => {
+  //           let newMovies = res2.data;
+  //           for (let j = 0; j < newMovies.length; j++) {
+  //             movieArray.push(newMovies[j].Title);
+  //           }
+  //           console.log(movieArray.sort());
+            
+  //         });
+  //     }
+      
+  //   });
 }
 
 fetchData('spiderman');
